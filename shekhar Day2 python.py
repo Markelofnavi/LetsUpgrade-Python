baab7e30c6d79486538ed18e37fb914c6a100661
Shekhar Suman <49583144+Markelofnavi@users.noreply.github.com>
2020-07-23 10:55:43 +0530

    Add files via upload

---
+++ shekhar Day2 python.py
@@ -0,0 +1,441 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+#Python basic concepts taught in Day2
+#Concepts practised by Shekhar Suman
+#Formatting
+
+
+# In[2]:
+
+
+print("Hello World")
+
+
+# In[4]:
+
+
+print("Hello")
+
+
+# In[6]:
+
+
+print("hello       python")
+
+
+# In[7]:
+
+
+"""       ("") ---- ("")
+('o')
+
+(") - (")
+(""')- (""')    """
+
+
+# In[113]:
+
+
+#Triple string
+
+
+# In[8]:
+
+
+print(" Python's world")
+
+
+# In[9]:
+
+
+print("Hello\n Python")
+
+
+# In[11]:
+
+
+print(' Python\'s world')
+
+
+# In[114]:
+
+
+# \ tells the interpreter that it is in English, no need to interfere with interpretation
+
+
+# In[12]:
+
+
+print(" Hello \a Python")
+
+
+# In[115]:
+
+
+#alert sound
+
+
+# In[15]:
+
+
+print('Python \'s world')
+
+
+# In[59]:
+
+
+name = "xyz"
+marks= 92.87
+age= 26
+print("The Name of person is", name,",Marks is", marks,",Age is", age)
+print("The Name of person is %2s, Marks is %2d, Age is %2d" %(name,marks,age))
+
+
+# In[60]:
+
+
+print(f" The name of person is {name} the marks is {marks} age is {age}")
+
+
+# In[ ]:
+
+
+#fstring applicable for version above 3.4
+
+
+# In[61]:
+
+
+x=y=10
+
+
+# In[62]:
+
+
+x
+
+
+# In[63]:
+
+
+y
+
+
+# In[64]:
+
+
+x=20
+id(x)
+
+
+# In[65]:
+
+
+y=20
+id(y)
+
+
+# In[116]:
+
+
+#Variable declaration
+
+
+# In[66]:
+
+
+del x
+
+
+# In[67]:
+
+
+x
+
+
+# In[68]:
+
+
+y
+
+
+# In[69]:
+
+
+#Assignment operator
+
+
+# In[70]:
+
+
+5**5
+
+
+# In[71]:
+
+
+10+50
+
+
+# In[72]:
+
+
+10-25
+
+
+# In[73]:
+
+
+10/3
+
+
+# In[74]:
+
+
+10%4
+
+
+# In[75]:
+
+
+#Comparision operator
+
+
+# In[76]:
+
+
+x=10
+
+
+# In[77]:
+
+
+y=20
+x==y
+
+
+# In[78]:
+
+
+z=10
+x==z
+
+
+# In[79]:
+
+
+z<=x
+
+
+# In[80]:
+
+
+x>=y
+
+
+# In[81]:
+
+
+x>y
+
+
+# In[82]:
+
+
+x!=y
+
+
+# In[83]:
+
+
+x = get_ipython().getoutput('y')
+
+
+# In[84]:
+
+
+#no error but wirong format
+
+
+# In[85]:
+
+
+#Bitwise operator
+
+
+# In[86]:
+
+
+a=245
+
+
+# In[87]:
+
+
+b=1
+
+
+# In[89]:
+
+
+a|b
+
+
+# In[90]:
+
+
+bin(a)
+
+
+# In[91]:
+
+
+bin(b)
+
+
+# In[92]:
+
+
+a&b
+
+
+# In[93]:
+
+
+#logical operator
+
+
+# In[94]:
+
+
+a=1
+b=20
+a>20
+
+
+# In[95]:
+
+
+a>20 or 10>9
+
+
+# In[96]:
+
+
+a>20 and 10>9
+
+
+# In[97]:
+
+
+10>9 and 20>M
+
+
+# In[98]:
+
+
+10<9 and 20>M
+
+
+# In[99]:
+
+
+not 10>9
+
+
+# In[100]:
+
+
+not True
+
+
+# In[101]:
+
+
+str1= "pythonprogramming"
+"a" in str1
+
+
+# In[103]:
+
+
+"x" not in str1
+
+
+# In[104]:
+
+
+"x" in str1
+
+
+# In[105]:
+
+
+x=1.5
+y= 1.5
+x==y
+
+
+# In[106]:
+
+
+x is y
+
+
+# In[107]:
+
+
+id(x)
+
+
+# In[108]:
+
+
+id(y)
+
+
+# In[109]:
+
+
+#different memory location
+
+
+# In[110]:
+
+
+10+10/29*4
+
+
+# In[111]:
+
+
+10/29
+
+
+# In[112]:
+
+
+10>8>9>11
+
+
+# In[117]:
+
+
+#10>8 and 8>9 and 9>11
+
+
+# In[118]:
+
+
+#DAY2 Finished
+
+
+# In[ ]:
+
+
+
+
