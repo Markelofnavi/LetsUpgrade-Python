85ae913ef40386fba7eb6c14fe75d0521e3afbe5
Shekhar Suman <49583144+Markelofnavi@users.noreply.github.com>
2020-08-01 21:41:01 +0530

    Add files via upload

---
+++ day 4 python shekhar.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[35]:
+
+
+#Day 4 assignment- shekhar suman
+
+
+# In[36]:
+
+
+#question1
+
+
+# In[37]:
+
+
+#To Find all the occurrence of substring in the given string
+
+
+# In[38]:
+
+
+
+givenstring="What we think we become; we are python programmer"
+for i in range(len(givenstring)):
+    if givenstring.startswith("we",i):
+        print("The start index of string are",i)
+
+
+# In[39]:
+
+
+#Isupper()
+
+
+# In[32]:
+
+
+str2="myworld"
+str2.isupper()
+
+
+# In[33]:
+
+
+str2="MYWORLD"
+str2.isupper()
+
+
+# In[34]:
+
+
+str2="Myworld"
+str2.isupper()
+
+
+# In[40]:
+
+
+#IsLower()
+
+
+# In[29]:
+
+
+
+str3 = "super"
+str3.islower()
+
+
+# In[30]:
+
+
+str3 = "Super"
+str3.islower()
+
+
+# In[31]:
+
+
+str3 = "SUPER"
+str3.islower()
+
+
+# In[ ]:
+
+
+
+
