8cf5a805feefe8b7e5c7a24670d2ac875a044190
Shekhar Suman <49583144+Markelofnavi@users.noreply.github.com>
2020-08-01 22:01:31 +0530

    Add files via upload

---
+++ day6 python shekhar.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+#day6 assignment- shekhar suman
+
+
+# In[10]:
+
+
+#question
+
+
+# In[8]:
+
+
+#List 1 and List 2 is given. Convert to Dictionary using list comprehension
+
+
+# In[11]:
+
+
+List1 = [1,2,3,4,5,7,8]
+List2 = ["a","b","c","d","e"]
+print(List1,List2)
+
+
+# In[12]:
+
+
+each={List1[r]:List2[r] for r in range(len(List2))}
+print(each)
+
+
+# In[ ]:
+
+
+
+
